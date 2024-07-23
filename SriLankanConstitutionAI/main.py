93a5cdf1c6af03d117b797d297f64d7cf57c9fd4
Kanishka Yohan <kanishka.gunawardana@gmail.com>
2024-07-23 12:00:20 +0530

    remote model access

--- SriLankanConstitutionAI/main.py
+++ SriLankanConstitutionAI/main.py
@@ -5,11 +5,18 @@ from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import OllamaEmbeddings
 import ollama
 import os
+from huggingface_hub import InferenceClient
 
-#instructions to start
-#https://www.linkedin.com/pulse/enhance-document-management-ai-extract-insights-from-pdfs-le-sueur-kfd5f/
-#https://github.com/RexiaAI/codeExamples/blob/main/localRAG/RAG.py
-#ollama pull nomic-embed-text
+# instructions to start
+# https://www.linkedin.com/pulse/enhance-document-management-ai-extract-insights-from-pdfs-le-sueur-kfd5f/
+# https://github.com/RexiaAI/codeExamples/blob/main/localRAG/RAG.py
+# ollama pull nomic-embed-text
+
+client = InferenceClient(
+    "meta-llama/Meta-Llama-3-8B-Instruct",
+    token="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",
+)
+
 
 # Load, split, and retrieve documents from a local PDF file
 def loadAndRetrieveDocuments() -> Chroma:
@@ -21,9 +28,11 @@ def loadAndRetrieveDocuments() -> Chroma:
     vectorStore = Chroma.from_documents(documents=documentSplits, embedding=embeddings)
     return vectorStore.as_retriever()
 
+
 # Format a list of documents into a string
 def formatDocuments(documents: list) -> str:
     return "\n\n".join(document.page_content for document in documents)
+
 
 # Define the RAG chain function
 def ragChain(question: str) -> str:
@@ -31,8 +40,22 @@ def ragChain(question: str) -> str:
     retrievedDocuments = retriever.invoke(question)
     formattedContext = formatDocuments(retrievedDocuments)
     formattedPrompt = f"Question: {question}\n\nContext: {formattedContext}"
-    response = ollama.chat(model='llama3', messages=[{'role': 'user', 'content': formattedPrompt}])
-    return response['message']['content']
+
+    '''response = ollama.chat(model='llama3', messages=[{'role': 'user', 'content': formattedPrompt}])
+    return response['message']['content']'''
+
+    response = client.chat_completion(
+        messages=[{"role": "user", "content": formattedPrompt}],
+        max_tokens=500,
+        stream=False
+    )
+    # Extract the generated response text using dataclass attributes
+    generated_text = ""
+    if response and response.choices:
+        generated_text = response.choices[0].message.content
+
+    return generated_text or "No response generated"
+
 
 # Gradio interface
 interface = gr.Interface(
